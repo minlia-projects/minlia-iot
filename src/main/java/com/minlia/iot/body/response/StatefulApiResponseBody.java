116276d8d790c93c1fc0b6e36d61d0a013eff7ed
will <will@minlia.com>
2017-09-21 03:11:38 +0800

    Update

--- src/main/java/com/minlia/iot/body/response/StatefulApiResponseBody.java
+++ src/main/java/com/minlia/iot/body/response/StatefulApiResponseBody.java
@@ -1,10 +1,12 @@
 package com.minlia.iot.body.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.minlia.cloud.body.Body;
 
 /**
  * Created by will on 9/10/17.
  */
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public interface StatefulApiResponseBody<RESPONSE> extends Body {
 
   Boolean isSuccess();
