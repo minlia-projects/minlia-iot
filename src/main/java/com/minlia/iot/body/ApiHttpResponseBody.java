ce58f31949907869f80677837753a266f5bb5085
will <will@minlia.com>
2017-09-21 06:23:10 +0800

    Initial Import

--- src/main/java/com/minlia/iot/body/ApiHttpResponseBody.java
+++ src/main/java/com/minlia/iot/body/ApiHttpResponseBody.java
@@ -1,5 +1,6 @@
 package com.minlia.iot.body;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.minlia.cloud.body.Body;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -7,6 +8,7 @@ import javax.xml.bind.annotation.XmlRootElement;
  * Created by will on 9/10/17.
  */
 @XmlRootElement(name = "xml")
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiHttpResponseBody implements Body {
 
 }
