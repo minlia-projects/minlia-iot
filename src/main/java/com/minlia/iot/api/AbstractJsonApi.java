1175c194f74e03ae4b6e748a3733bca540cd619a
will <will@minlia.com>
2017-09-25 20:16:38 +0800

    Update

---
+++ src/main/java/com/minlia/iot/api/AbstractJsonApi.java
@@ -0,0 +1,44 @@
+package com.minlia.iot.api;
+
+import com.minlia.iot.config.ApiCredentialConfiguration;
+import com.minlia.iot.config.ApiEndpointConfiguration;
+import com.minlia.iot.context.ApiRuntimeContext;
+import com.minlia.iot.http.JsonApiHttpExecutor;
+import com.minlia.iot.listener.ApiClientListener;
+import com.minlia.iot.listener.ApiClientListenerAdapter;
+import com.minlia.iot.marshal.deserialize.JsonApiDeserializer;
+import com.minlia.iot.marshal.serialize.JsonApiSerializer;
+import com.minlia.iot.requestor.HttpRequestMethod;
+import com.minlia.iot.scope.HttpMediaType;
+import java.util.Optional;
+
+/**
+ * Created by will on 9/17/17.
+ */
+public abstract class AbstractJsonApi extends AbstractApi {
+
+  public AbstractJsonApi sandbox(Boolean sandbox) {
+    apiRuntimeContext.setSandbox(sandbox);
+    return this;
+  }
+
+  /**
+   * 总个数只能为环境的个数, 如Production, Sandbox
+   * 如果一个都没有则报错
+   */
+  public AbstractJsonApi(ApiCredentialConfiguration[] apiCredentialConfiguration,
+      ApiEndpointConfiguration[] apiEndpointConfiguration) {
+    super(apiCredentialConfiguration,apiEndpointConfiguration);
+
+    //设置为JSON
+
+    apiRuntimeContext.setHttpMediaType(HttpMediaType.Json);
+    apiRuntimeContext.setApiDeserializer(new JsonApiDeserializer<>());
+    apiRuntimeContext.setApiSerializer(new JsonApiSerializer<>());
+    apiRuntimeContext.setApiHttpExecutor(new JsonApiHttpExecutor());
+
+
+  }
+
+
+}
---
+++ src/main/java/com/minlia/iot/code/IotApiCode.java
@@ -0,0 +1,9 @@
+package com.minlia.iot.code;
+
+/**
+ * Created by will on 9/22/17.
+ */
+public class IotApiCode {
+
+  public static final int NOT_NULL=10001;
+}
